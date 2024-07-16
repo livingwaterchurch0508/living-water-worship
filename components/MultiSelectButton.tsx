30d67b9fe3b712c81e109b8eea2e46218842323b
bwkim <bongborobong@gimbong-uui-MacBookPro.local>
2024-07-16 22:03:23 +0900

    - 이번주 재생하기

--- components/MultiSelectButton.tsx
+++ components/MultiSelectButton.tsx
@@ -1,11 +1,47 @@
 "use client";
 
-import { Box, Button } from "@chakra-ui/react";
 import React from "react";
+import { Box, Button } from "@chakra-ui/react";
+import { useRouter } from "next/navigation";
+
 import { usePressStore } from "@/store/press-store";
+import { TAB_TYPES } from "@/variables/enums";
+import { hymns } from "@/mock/hymns";
+import { pastors } from "@/mock/pastors";
+import { michaels } from "@/mock/michaels";
+import { prints } from "@/mock/prints";
 
 export default function MultiSelectButton() {
+  const router = useRouter();
+
   const { setEnabledMultiSelect } = usePressStore((state) => state);
+  const { allCheckedItems } = usePressStore((state) => state);
+
+  const playThisWeek = () => {
+    allCheckedItems({
+      filteredList: hymns.filter(({ isThisWeek = false }) => isThisWeek),
+      isAllChecked: true,
+      tab: TAB_TYPES.HYMNS,
+    });
+    allCheckedItems({
+      filteredList: pastors.filter(({ isThisWeek = false }) => isThisWeek),
+      isAllChecked: true,
+      tab: TAB_TYPES.PASTOR,
+    });
+    allCheckedItems({
+      filteredList: michaels.filter(({ isThisWeek = false }) => isThisWeek),
+      isAllChecked: true,
+      tab: TAB_TYPES.MICHAELS,
+    });
+    allCheckedItems({
+      filteredList: prints.filter(({ isThisWeek = false }) => isThisWeek),
+      isAllChecked: true,
+      tab: TAB_TYPES.PRINTS,
+    });
+    setTimeout(() => {
+      router.push("/multi/play");
+    }, 200);
+  };
 
   return (
     <Box
@@ -18,6 +54,9 @@ export default function MultiSelectButton() {
       alignItems="center"
       gap="1rem"
     >
+      <Button bg="#dfd3e4" size="sm" onClick={playThisWeek}>
+        이번주 재생하기
+      </Button>
       <Button
         colorScheme="gray"
         size="sm"
